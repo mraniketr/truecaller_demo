f4d808ebc98da9e52f736b08fc74bc0433660f54
Aniket Rai <im.aniket.rai@gmail.com>
2021-05-08 11:21:30 +0530

    rest api updates

--- truecaller/src/main/java/com/aniket/truecaller/controller/UserController.java
+++ truecaller/src/main/java/com/aniket/truecaller/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.aniket.truecaller.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aniket.truecaller.userRepo;
@@ -18,5 +22,20 @@ public class UserController {
 	public List<users> getusers() {
 		return repo.findAll();
 	}
+	@PostMapping("/users")
+	public String addUser(@RequestBody users u1) {
+		repo.save(u1);
+		return "Successfully added";
+	}
+	@GetMapping("/users/id/{id}")
+	public Optional<users> getuserbyID(@PathVariable int id) {
+		
+		return repo.findById(id);
+	}
+//	@GetMapping("/users/phone/{phone}")
+//	public Optional<users> getuserbyphone(@PathVariable String phone) {
+//		
+////		return repo.findBy(phone);
+//	}
 }
 
--- truecaller/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `users`(`name`, `phone`, `email`) VALUES ('Aniket','12346685','a@g.co');
