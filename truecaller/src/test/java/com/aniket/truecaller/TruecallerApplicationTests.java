f739c429cf1366c5aa84447c9d75c5371efa9b19
Aniket Rai <im.aniket.rai@gmail.com>
2021-05-08 09:41:33 +0530

    init commit

---
+++ truecaller/src/test/java/com/aniket/truecaller/TruecallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniket.truecaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruecallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
