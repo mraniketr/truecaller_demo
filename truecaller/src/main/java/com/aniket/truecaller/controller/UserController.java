91c620e209e4ed0b4de1c311612f5f1833ef02f8
Aniket Rai <im.aniket.rai@gmail.com>
2021-05-10 23:32:13 +0530

    rest api completed

--- truecaller/src/main/java/com/aniket/truecaller/controller/UserController.java
+++ truecaller/src/main/java/com/aniket/truecaller/controller/UserController.java
@@ -4,19 +4,25 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.aniket.truecaller.userRepo;
 import com.aniket.truecaller.model.users;
 
+//@Controller
 @RestController
 public class UserController {
 	@Autowired
 	private userRepo repo;
+	
 	
 	@GetMapping("/users")
 	public List<users> getusers() {
@@ -27,15 +33,26 @@ public class UserController {
 		repo.save(u1);
 		return "Successfully added";
 	}
+	
 	@GetMapping("/users/id/{id}")
 	public Optional<users> getuserbyID(@PathVariable int id) {
-		
 		return repo.findById(id);
+		
+	}	
+	@GetMapping("/users/phone/{phone}")
+	public List<users> getuserbyphone(@PathVariable String phone) {
+		
+		return repo.findByphone(phone);
 	}
-//	@GetMapping("/users/phone/{phone}")
-//	public Optional<users> getuserbyphone(@PathVariable String phone) {
-//		
-////		return repo.findBy(phone);
-//	}
+	@GetMapping("/users/name/{name}")
+	public List<users> getuserbyname(@PathVariable String name) {
+		
+		return repo.findByname(name);
+	}
+	@PutMapping("/users")
+	public String updateUser(@RequestBody users s1) {
+		repo.save(s1);
+		return "Successfully added";
+	}
 }
 
--- truecaller/src/main/java/com/aniket/truecaller/userRepo.java
+++ truecaller/src/main/java/com/aniket/truecaller/userRepo.java
@@ -1,9 +1,17 @@
 package com.aniket.truecaller;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.aniket.truecaller.model.users;
 
 public interface userRepo extends JpaRepository<users, Integer>{
+
+	List<users> findByphone(String phone);
+	
+	@Query("from users where name like %?1%")
+	List<users> findByname(String name);
 
 }
